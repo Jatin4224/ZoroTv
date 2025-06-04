a524f0c41ddba6017d4e41e62dae1a27433e5e8f
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-06-04 15:41:27 +0530

    added zoroicon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,15 @@
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div>
+      <div className="flex justify-start items-center gap-4 h-20 p-12 ml-10">
+        <img
+          src="https://zorotv.com.in/wp-content/uploads/2024/03/zorotv.png"
+          className="h-15 w-40 "
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Sidebar;
