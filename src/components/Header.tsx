949de3b9e98ad8a309b7b2d5da9bb8a5377d67b4
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-06-06 22:36:19 +0530

    created header wallpaper

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+interface wallpaperProp {
+  wallpaper: string;
+}
+const Header = ({ wallpaper }: wallpaperProp) => {
+  console.log(wallpaper);
+  return (
+    <div
+      style={{
+        background: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.7),rgba(0,0,0,.9)), url(https://image.tmdb.org/t/p/original/${
+          wallpaper.backdrop_path || wallpaper.profile_path
+        })`,
+        backgroundSize: "cover",
+        backgroundPosition: "center ",
+      }}
+      className="text-white bg-transparent shadow-[0_4px_20px_rgba(0,255,0,0.6)] h-[50vh] w-[70vw] -translate-y-40 translate-x-10 opacity-80 
+             transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-[0_4px_30px_rgba(0,255,0,0.8)] cursor-pointer rounded-xl"
+    >
+      Header
+    </div>
+  );
+};
+
+export default Header;
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import MainScreen from "./MainScreen";
 import Sidebar from "./Sidebar";
+import axios from "../utils/axios";
+import Header from "./Header";
 
 const Home = () => {
+  const [wallpaper, setWallpaper] = useState(null);
+
+  const GetHeaderWallpaper = async () => {
+    try {
+      let wallpaperData = await axios.get(`/trending/all/day`);
+
+      let randomValue =
+        wallpaperData.data.results[
+          (Math.random() * wallpaperData.data.results.length).toFixed()
+        ];
+      setWallpaper(randomValue);
+    } catch (error) {
+      console.log("fetching GetHeaderWallpaper data failed", error);
+    }
+  };
+
+  useEffect(() => {
+    !wallpaper && GetHeaderWallpaper();
+  }, []);
+
   return (
     <>
       <div className="flex h-180  bg-[url('https://4kwallpapers.com/images/wallpapers/roronoa-zoro-one-1920x1080-10523.png')] bg-cover  relative ">
@@ -10,6 +33,7 @@ const Home = () => {
         </div>
         <div className="bg-[#1FE24] w-[75%] h-screen ">
           <MainScreen />
+          <Header wallpaper={wallpaper} />
         </div>
       </div>
     </>
