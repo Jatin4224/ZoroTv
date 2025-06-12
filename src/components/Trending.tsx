9f42c21e3d7d31e66a2efb7260a1bc4bf392b3ec
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-06-12 20:25:21 +0530

    infinite-scroll

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -9,6 +9,7 @@ import Loading from "./Loading";
 const title = ["categories", "Duration"];
 const categories = ["movie", "tv", "all"];
 const durations = ["week", "day"];
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Trending = () => {
   const navigate = useNavigate();
@@ -16,12 +17,22 @@ const Trending = () => {
   const [category, setCategory] = useState("all");
   const [duration, setDuration] = useState("day");
   const [trending, setTrending] = useState(null);
-
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const GetTrending = async () => {
     try {
-      const trendingData = await axios.get(`/trending/${category}/${duration}`);
+      const trendingData = await axios.get(
+        `/trending/${category}/${duration}?page=${page}`
+      );
+      const newResults = trendingData.data.results;
 
       setTrending(trendingData.data.results);
+      if (res.data.results.length === 0) {
+        setHasMore(false);
+      }
+      setTrending((prev) =>
+        page === 1 ? newResults : [...prev, ...newResults]
+      );
     } catch (error) {
       console.log("fetching GetHeaderWallpaper data failed", error);
     }
@@ -29,14 +40,19 @@ const Trending = () => {
   console.log(trending);
 
   useEffect(() => {
-    GetTrending();
+    setPage(1);
+    setTrending(null);
   }, [category, duration]);
+
+  useEffect(() => {
+    GetTrending();
+  }, [category, duration, page]);
 
   if (!trending) {
     return <Loading />;
   }
   return (
-    <div className="w-screen h-screen p-[2%] bg-[url('./assets/zoro3.png')] bg-center bg-cover">
+    <div className="w-screen h-screen p-[2%] bg-[url('./assets/zoro3.png')] bg-center bg-cover overflow-hidden overflow-y-auto">
       <div className="w-full h-[10vh] flex flex-row items-center  gap-6">
         <FaArrowLeft
           onClick={() => navigate(-1)}
@@ -57,7 +73,14 @@ const Trending = () => {
           func={(e) => setDuration(e.target.value)}
         />
       </div>
-      <Cards data={trending} title={category} />
+      <InfiniteScroll
+        dataLength={trending.length}
+        next={() => setPage((prev) => prev + 1)}
+        hasMore={hasMore}
+        loader={<Loading />}
+      >
+        <Cards data={trending} title={category} />
+      </InfiniteScroll>
     </div>
   );
 };
